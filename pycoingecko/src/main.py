e7fa018ae67179339a41bcfa079f9b50465d6638
Assylken <asylken02@gmail.com>
2021-09-18 00:20:40 +0600

    bugs fixed

--- pycoingecko/src/main.py
+++
@@ -1,9 +0,0 @@
-from pycoingecko import CoinGeckoAPI
-cg = CoinGeckoAPI()
-def filter(n):
-	def comp(list):
-    		return list['market_cap_rank']
-
-	coinList = cg.get_coins_markets('usd')
-	coinList.sort(key = comp)
-	return coinList[:n]
