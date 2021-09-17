9a191660117fc540d3f1d8ddff3dbdb227bce832
Assylken <79912262+Assylken@users.noreply.github.com>
2021-09-18 00:05:24 +0600

    Add files via upload
    Python code pulls data from CoinGecko API, filter function

---
+++ pycoingecko/test/test.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.insert(0, '/Users/asylk/Desktop/CoinGecko/pycoingecko/src')
+  
+from main import filter
+
+n = int(input('Enter a number to output top N cryptocurrencies: '))
+print()
+coinList = filter(n)
+for val in coinList:
+	print(val['name'], "| Price: ", val['current_price'], "$ | Market_Cap: ", val['market_cap'])
